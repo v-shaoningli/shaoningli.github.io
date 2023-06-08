18b11e09e789f3f84b5e676b8ee3fb740a96a90f
v-shaoningli <shaoningli5190@gmail.com>
2023-06-07 22:32:00 -0700

    del unused

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,22 @@ import {
   Container,
   Heading,
   Box,
-  SimpleGrid,
+  // SimpleGrid,
   Button,
   List,
   ListItem,
-  useColorModeValue,
+  // useColorModeValue,
   chakra
 } from '@chakra-ui/react'
-import { ChevronRightIcon, EmailIcon } from '@chakra-ui/icons'
-import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons' // EmailIcon
+// import Paragraph from '../components/paragraph'
 import { BioSection, BioYear, PubSection } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { GridItem } from '../components/grid-item'
-import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
-import thumbYouTube from '../public/images/links/youtube.png'
-import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
+// import { GridItem } from '../components/grid-item'
+import { IoLogoGithub } from 'react-icons/io5' // IoLogoTwitter, IoLogoInstagram
+// import thumbYouTube from '../public/images/links/youtube.png'
+// import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
 import Image from 'next/image'
 
 const ProfileImage = chakra(Image, {
