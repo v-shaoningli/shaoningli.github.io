db7cda024b58a04dc001e628c3d1e74643373b9c
v-shaoningli <v-shaoningli@microsoft.com>
2023-08-02 00:17:14 -0700

    change name

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} Takuya Matsuyama. All Rights Reserved.
+      Thanks to &copy; {new Date().getFullYear()} Takuya Matsuyama. All Rights Reserved.
     </Box>
   )
 }
--- components/layouts/article.js
+++ components/layouts/article.js
@@ -9,7 +9,7 @@ const variants = {
 }
 
 const Layout = ({ children, title }) => {
-  const t = `${title} - Takuya Matsuyama`
+  const t = `${title} - Shaoning`
   return (
     <motion.article
       initial="hidden"
